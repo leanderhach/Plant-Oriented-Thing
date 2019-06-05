841e5d42229b91cbaacb6b84de135aa9fa43561d
Leander Hach <leander.hach@gmail.com>
2019-06-05 11:37:51 +0200

    create vue-cli project, add files

---
+++ vue-pot/src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>
+      <span>P</span>
+      <span>.</span>
+      <span>O</span>
+      <span>.</span>
+      <span>T</span>
+    </h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
