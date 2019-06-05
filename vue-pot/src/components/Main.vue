f544b4ac6be1ac26062b1736c70250aa39c48442
Leander Hach <leander.hach@gmail.com>
2019-06-05 14:49:44 +0200

    .

--- vue-pot/src/components/Main.vue
+++ vue-pot/src/components/Main.vue
@@ -7,11 +7,22 @@
       <span>.</span>
       <span>T</span>
     </h1>
+    <p>Welcome to POT. This is where you can change the conditions inside your POT and see how your plant is doing.</p>
+
+    <input type="range" name="brightness">
+    <label for="brightness"></label>
   </main>
 </template>
 
 <script>
 export default {
   name: 'Main',
+  data() {
+    return {
+      env_data: {
+        
+      }
+    }
+  },
 }
 </script>
--- vue-pot/src/main.js
+++ vue-pot/src/main.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAEKZ4wSppqPBrCtQ04sx5HrtW3gwtDgoQ",
+  authDomain: "p-o-t-242713.firebaseapp.com",
+  databaseURL: "https://p-o-t-242713.firebaseio.com",
+  projectId: "p-o-t-242713",
+  storageBucket: "p-o-t-242713.appspot.com",
+  messagingSenderId: "764642598866",
+  appId: "1:764642598866:web:004d62fc7e720d46"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
