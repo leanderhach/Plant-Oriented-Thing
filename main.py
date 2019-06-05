f6ecd0ef35a4220b4309aa132684e15eb7346df1
Leander Hach <leander.hach@gmail.com>
2019-06-05 13:46:11 +0200

    add camera code and update main.py with list of things to do

---
+++ main.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+
+#MAIN LOOP
+while True:
+
+    #TODO:
+    # - add light function call
+    # - add sensor function call
+    # - add camera function call
+    # - add call for speakers based on camera call return
+
+
+    # sleep for a second, then repeat program
+    sleep(1)
