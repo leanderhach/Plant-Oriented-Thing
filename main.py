a95453f3b0e879b7422f5289fadc3c3314b0b109
Leander Hach <leander.hach@gmail.com>
2019-06-07 21:05:52 +0200

    added descriptive comments to main

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from time import sleep
 from api import setup_db_access, api_main
 from repeating_thread import RepeatingThread
 from sensors import sensors_main
-#MAIN LOOP
+
+
+"""Main loop
+
+    main.py is responsible for starting every _main() function in each of the other python files.
+    It does this by creating a new instace of the RepeatingThread class for each _main(),
+
+"""
 
 #TODO:
 # - add light function call
-# - add sensor function call
 # - add camera function call
-# - add call for speakers based on camera call return
+# - add ability to shut down program greacefully
 #####################################################
 
 ## access database and start loop
 setup_db_access()
 RepeatingThread(5, api_main)
 RepeatingThread(5, sensors_main)
-
-## start sensors
-# sensors_main()
-
-
-# sleep for a second, then repeat program
