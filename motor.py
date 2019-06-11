077534e89c8b91dff31dec40702084175f17e895
pandamarty <51188801+pandamarty@users.noreply.github.com>
2019-06-11 11:15:54 +0200

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,6 +1,8 @@
 import explorerhat as ex
 from time import sleep
 
+#we need to have the motor pump 50cl at a time, still need to figure out seconds to do that
+
 def start(seconds):
     ex.motor.one.forward(90)
     sleep(seconds)
@@ -8,3 +10,4 @@ def start(seconds):
 
 def stop():
     ex.motor.one.stop()
+   
