37084d7d1ffd65926ae668892cc5ea47a25fa935
GilbertRoss <bhuiyannazrul74@gmail.com>
2019-06-12 17:41:05 +0200

    update sensor.py

--- sensors.py
+++ sensors.py
@@ -18,14 +18,14 @@ def sensors_main():
 
         
     if (value <= 0.2):
-        f = open("myfile.txt", "a") #creates myfile.txt
-        f.write(now.day) #writes which day is today
+        if (!f.exists):
+            f = open("myfile.txt", "a") #creates myfile.txt
+            f.write(now.day) #writes which day is today
         fileHandle = open ( 'myfile.txt',"r" ) #opens the file previously created
-        lineList = fileHandle.readlines() #reads the lines
-        fileHandle.close()
-        fileDate =  lineList[-1] #creates a variable fileDate and stores the date of the last line
-        if((now.day-fileDate) == 3): # if 3 days had passed it changes the leds color
-            set_led(brightness=1, color=file.read_env_data("led_warning_color"))
+            line = fileHandle.readlines() #reads the lines
+            fileHandle.close()
+            if((now.day-line) == 3): # if 3 days had passed it changes the leds color
+                set_led(brightness=1, color=file.read_env_data("led_warning_color"))    
 
     else:
         set_led(brightness= median(), color=file.read_env_data("led_grow_color"))
